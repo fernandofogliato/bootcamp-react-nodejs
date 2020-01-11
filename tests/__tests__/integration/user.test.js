410cf2c8734be7400d3ad89948ad52c05509d5fb
Fernando Fogliato <fernandofogliato@gmail.com>
2020-01-11 00:04:53 -0300

    Add User tests

--- tests/__tests__/integration/user.test.js
+++ tests/__tests__/integration/user.test.js
@@ -1,6 +1,8 @@
+import bcrypt from 'bcryptjs';
 import request from 'supertest';
 import app from '../../src/app';
 
+import factory from '../factories';
 import truncate from '../util/truncate';
 
 describe('User', () => {
@@ -8,35 +10,35 @@ describe('User', () => {
     await truncate();
   });
 
+  it('should encrypt user password when new user created', async () => {
+    const user = await factory.create('User');
+
+    const compareHash = await bcrypt.compare(user.password, user.password_hash);
+    expect(compareHash).toBe(true);
+  });
+
   it('should be able to register', async () => {
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Teste',
-        email: 'teste@teste.com.br',
-        password_hash: '123456',
-      });
+      .send(user);
 
     expect(response.body).toHaveProperty('id');
   });
 
   it('should not be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User', {
+      email: 'teste@teste.com.br',
+    });
+
     await request(app)
       .post('/users')
-      .send({
-        name: 'Teste',
-        email: 'teste@teste.com.br',
-        password_hash: '123456',
-      });
+      .send(user);
 
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Teste',
-        email: 'teste@teste.com.br',
-        password_hash: '123456',
-      });
-
+      .send(user);
     expect(response.status).toBe(400);
   });
 });
--- tests/src/database/migrations/20200111010927-create-users.js
+++ tests/src/database/migrations/20200111010927-create-users.js
@@ -23,6 +23,10 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false,
       },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
